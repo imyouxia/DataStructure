6892b84ba1356d447ff29f158b3a322eab0f1b7c
armsword <root@linuxer.me>
2014-02-22 20:03:30 +0800

    后序遍历方式

--- BinaryTree/postorder.c
+++ BinaryTree/postorder.c
@@ -1,4 +1,4 @@
-/* 二叉树前序遍历方式 */
+/* 二叉树后序遍历方式 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -70,18 +70,19 @@ btree createbtree(int *data,int len)
 }
 
 
-/* 二叉树前序遍历 */
-void preorder(btree ptr)
+/* 二叉树后序遍历 */
+void postorder(btree ptr)
 {
 	if(ptr != NULL)  //终止条件
 	{
+		postorder(ptr->left);  //左子树
+		postorder(ptr->right);   //右子树
 		printf("[%2d]\n",ptr->data);   //输出结点内容
-		preorder(ptr->left);  //左子树
-		preorder(ptr->right);   //右子树
+	
 	}
 }
 
-/* 主程序：创建二叉树且用前序遍历输出 */
+/* 主程序：创建二叉树且用后序遍历输出 */
 int main()
 {
 	btree root = NULL;   //树根指针
@@ -90,7 +91,7 @@ int main()
 	int data[9] = {5,6,4,8,2,3,7,1,9};
 	root = createbtree(data,9);
 	printf("树的结点内容\n");
-	preorder(root);   //前序遍历二叉树
+	postorder(root);   //后序遍历二叉树
 	return 0;
 
 }
