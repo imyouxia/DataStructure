6da124b351066c5fb15d72aa71edb57fbf31813c
armsword <root@linuxer.me>
2014-04-06 17:27:51 +0800

    字符串变为整型

---
+++ StrToInt/StrToInt.c
@@ -0,0 +1,65 @@
+/*《剑指offer》里，把字符串变为int类型的一个类似atoi()函数
+
+*/
+
+enum Status {kValid = 0 ,kInvalid};
+int g_nStatus = kValid;
+
+int StrToInt(const char* str)
+{
+	g_nStatus = kInvalid;
+	long long num = 0;
+	if(str != NULL && *str != '\0')
+	{
+		bool minus = false;
+		if(*str == '+')
+			str++;
+		else if(*str == '-')
+		{
+			str++;
+			minus = true;
+		}
+		if(*str != '\0')
+		{
+			num = StrToIntCore(str,minus);
+		}
+	}
+	return (int)num;
+}
+
+long long StrToIntCore(const char* digit,bool minus)
+{
+	long long num = 0;
+	
+	while(*digit != '\0')
+	{
+		if(*digit >='0' && *digit <= '9')
+		{
+			int flag = minus ? -1 : 1;
+			num = num * 10 + flag * (*digit - '0');
+			if((!minus && num > 0x7FFFFFFF) || (minus && num < (signed int)0x80000000))
+			{
+				num = 0;
+				break;
+			}
+			digit++;
+		}
+		else
+		{
+			num = 0;
+			break;
+		}
+	}
+
+	if(*digit == '\0')
+	{
+		g_nStatus = kValid;
+	}
+
+	return num;
+}
+
+
+
+
+
